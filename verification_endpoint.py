4bb854f9baa1e78f0a781c034394fe8e5d843ae6
Dan Luo <luo_dana@yahoo.com>
2023-03-28 11:34:01 +1000

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -13,7 +13,8 @@ def verify():
     content = request.get_json(silent=True)
 
 # YOUR CODE BELOW
-    
+    Print('start checking')
+    Print(content)
     if 'sig' not in content:
         result = False
         return jsonify(result)

97f80a9dd412dd3042b8ea00b596ec8ff1012a38
Dan Luo <luo_dana@yahoo.com>
2023-03-28 11:01:24 +1000

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,13 +11,54 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
+# YOUR CODE BELOW
 
+
+    # Extract payload and signature from request
+    payload = content['payload']
+    signature = content['sig']
+
+    # Serialize payload dictionary to a string
+    payload_str = json.dumps(payload, sort_keys=True)
+
+    # Check platform to use appropriate verification algorithm
+    platform = payload['platform']
+    if platform == 'Ethereum':
+        # Extract public key from payload and convert to lowercase
+        pk = payload['pk'].lower()
+
+        # Hash payload string using Ethereum message encoding
+        message = eth_account.messages.encode_defunct(text=payload_str)
+
+        try:
+            # Verify signature using Ethereum account library
+            eth_account.Account.recover_message(message, signature=signature) == pk
+            return jsonify(True)
+        except:
+            return jsonify(False)
+    elif platform == 'Algorand':
+        # Extract public key from payload
+        pk = payload['pk']
+
+        # Convert payload string to bytes
+        message = payload_str.encode('utf-8')
+
+        try:
+            # Verify signature using Algorand encoding library
+            public_key = algosdk.encoding.decode_address(pk)
+            algosdk.sign.verify(message, signature, public_key)
+            return jsonify(True)
+        except:
+            return jsonify(False)
+    else:
+        # Invalid platform
+        return jsonify(False)
 
 
 
     #Check if signature is valid
-    result = True #Should only be true if signature validates
-    return jsonify(result)
+    #result = True #Should only be true if signature validates
+    #return jsonify(result)
 
 if __name__ == '__main__':
     app.run(port='5002')

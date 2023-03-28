acef6dd09e6962d0de196adead655eba6af8af81
Dan Luo <luo_dana@yahoo.com>
2023-03-28 11:33:09 +1000

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,54 +11,57 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
+
 # YOUR CODE BELOW
-
-
-    # Extract payload and signature from request
-    payload = content['payload']
-    signature = content['sig']
-
-    # Serialize payload dictionary to a string
-    payload_str = json.dumps(payload, sort_keys=True)
-
-    # Check platform to use appropriate verification algorithm
-    platform = payload['platform']
-    if platform == 'Ethereum':
-        # Extract public key from payload and convert to lowercase
-        pk = payload['pk'].lower()
-
-        # Hash payload string using Ethereum message encoding
-        message = eth_account.messages.encode_defunct(text=payload_str)
-
-        try:
-            # Verify signature using Ethereum account library
-            eth_account.Account.recover_message(message, signature=signature) == pk
-            return jsonify(True)
-        except:
-            return jsonify(False)
-    elif platform == 'Algorand':
-        # Extract public key from payload
-        pk = payload['pk']
-
-        # Convert payload string to bytes
-        message = payload_str.encode('utf-8')
-
-        try:
-            # Verify signature using Algorand encoding library
-            public_key = algosdk.encoding.decode_address(pk)
-            algosdk.sign.verify(message, signature, public_key)
-            return jsonify(True)
-        except:
-            return jsonify(False)
+    
+    if 'sig' not in content:
+        result = False
+        return jsonify(result)
+    if 'payload' not in content:
+        result = False
+        return jsonify(result)
+    if 'message' or 'pk' or 'platform' not in content['payload']:
+        result = False
+        return jsonify(result)
+    if content['payload']['platform'] == 'Ethereum' or 'Algorand':
+        signature_type = 'Ethereum'
+    elif content['payload']['platform'] == 'Algorand':
+        signature_type = 'Algorand'
     else:
-        # Invalid platform
-        return jsonify(False)
+        result = False
+        return jsonify(result)
 
+    signature = content['sig']
+    payload = json.dump(content['payload'])
 
+    #-----------verify ETH--------------------
+
+    eth_account.Account.enable_unaudited_hdwallet_features()
+    acct, mnemonic = eth_account.Account.create_with_mnemonic()
+
+    eth_pk = acct.address
+    eth_sk = acct.key
+
+    eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+    eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,eth_sk)
+    if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == eth_pk:
+        print( "ETH sig verifies!" )
+        result = True #Should only be true if signature validates
+        return jsonify(result)
+
+    #-----------verify algosdk--------------------
+    algo_sk, algo_pk = algosdk.account.generate_account()
+    algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'),algo_sk)
+
+    if algosdk.util.verify_bytes(payload.encode('utf-8'),algo_sig_str,algo_pk):
+        print( "Algo sig verifies!" )
+        result = True #Should on
+        return jsonify(result)
+    
 
     #Check if signature is valid
-    #result = True #Should only be true if signature validates
-    #return jsonify(result)
+    result = False #Should only be true if signature validates
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(port='5002')
